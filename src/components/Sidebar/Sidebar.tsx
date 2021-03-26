a0fc26b15bb056db2af9b77477c8386c9554ceda
Adson-Macedo <agnsoft@hotmail.com>
2021-03-25 23:55:10 -0300

    Implementacao do RoundedButton

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,28 +1,53 @@
 import React, { useState } from 'react'
-import { SidebarData } from './SidebarData'
 import styles from './Sidebar.module.css'
+
+const sidebarData = [
+    {
+        title: 'Início',
+        path: '/home',
+        icon: <img src="/icons/botao-home.svg" alt="Início" />,
+    },
+    {
+        title: 'Meus Grupos de Estudos',
+        path: '/grupos_estudos',
+        icon: <img src="/icons/group.svg" alt="Meus grupos de estudos" />,
+    },
+    {
+        title: 'Meus Questionários',
+        path: '/questionarios',
+        icon: <img src="/icons/questionario.svg" alt="Questionarios" />,
+    },
+    {
+        title: 'Dados Cadastrais',
+        path: '/dados-cadastrais',
+        icon: <img src="/icons/users.svg" alt="Meus grupos de estudos" />,
+    },
+    {
+        title: 'Ajuda',
+        path: '/ajuda',
+        icon: <img src="/icons/help.svg" alt="Meus grupos de estudos" />,
+    },
+]
 
 function Sidebar() {
     return (
-        <>
-            <nav className={styles.nav_menu}>
-                <div className={styles.nav_menu_img}>
-                    <img src="/QuestsApp-logo.svg" alt="Logo QuestsApp" />
-                </div>
-                <ul className={styles.nav_menu_items}>
-                    {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={styles.nav_text}>
-                                <a href="#">
-                                    {item.icon}
-                                    {item.title}
-                                </a>
+        <nav className={styles.nav_menu}>
+            <div className={styles.nav_menu_img}>
+                <img src="/QuestsApp-logo.svg" alt="Logo QuestsApp" />
+            </div>
+            <ul className={styles.nav_menu_items}>
+                {sidebarData.map((item, index) => {
+                    return (
+                        <a href="#" className={styles.nav_text}>
+                            <li key={index} >
+                                {item.icon}
+                                {item.title}
                             </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-        </>
+                        </a>
+                    )
+                })}
+            </ul>
+        </nav>
     )
 }
 
