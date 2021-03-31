f5faf4573bf429fa613f112c112eb96df8ec5379
Thairam <thayram01@gmail.com>
2021-03-31 20:15:48 -0300

    Implementação do Card: Grupo de Estudo

---
+++ src/components/base/CardGroup.tsx
@@ -0,0 +1,38 @@
+import { group } from 'node:console'
+import React, { Component } from 'react'
+import Group from '../../model/Group'
+
+import styles from './CardGroup.module.css'
+
+interface IGroupProps {
+    group: Group
+}
+
+export default class CardGroup extends Component<IGroupProps, {}> {
+
+    render(): any {
+
+        const group: Group = this.props.group
+
+        return (
+            <div className={styles.card_group}>
+
+                <div className={styles.container_img}>
+                    <img src='icons/group.svg' className={styles.img}></img>
+                </div>
+
+                <div className={styles.title_group}>
+                    <span>{group.descricao}</span>
+                </div>
+
+                <div className={styles.informations}>
+                    <span>{`${group.getMembersCount()} Membros`}</span>
+                    <span>{`${group.getQuestionnairesCount()} Questionários`}</span>
+                    <span>57 Questões</span>
+                </div>
+
+                <div className={styles.effect} />
+            </div>
+        )
+    }
+}
---
+++ src/model/Group.ts
@@ -0,0 +1,37 @@
+export default class Group {
+    private _descricao: string
+    private _members: Array<Number>
+    private _questionnaires: Array<Number>
+
+    public get descricao(): string {
+        return this._descricao
+    }
+
+    public get members(): Array<Number> {
+        return this._members
+    }
+
+    public get questionnaires(): Array<Number> {
+        return this._questionnaires
+    }
+
+    public set descricao(descricao: string) {
+        this._descricao = descricao;
+    }
+
+    public set members(members: Array<Number>) {
+        this._members = members;
+    }
+
+    public set questionnaires(questionnaires: Array<Number>) {
+        this._questionnaires = questionnaires;
+    }
+
+    public getMembersCount(): Number {
+        return this._members.length
+    }
+
+    public getQuestionnairesCount(): Number {
+        return this._questionnaires.length
+    }
+}
