7011b3f63e90d776e28d1acf32953a906ba37a1f
Ramon <rrsalescc@hotmail.com>
2021-04-01 22:16:12 -0300

    Ajustes nos itens selecionaveis da sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,28 @@
 import React, { useState, useContext } from 'react'
 import styles from './Sidebar.module.css'
-
 import templateData from '../../../template.json'
+import { useRouter } from 'next/router'
 
 function Sidebar() {
+
+    const router = useRouter()
+
     return (
         <nav className={styles.nav_menu}>
             <div className={styles.nav_menu_img}>
                 <img src="/QuestsApp-logo.svg" alt="Logo QuestsApp" />
             </div>
             <ul className={styles.nav_menu_items}>
-                {Object.values(templateData).map((item) => {
+                {Object.values(templateData).map((item, index) => {
                     return (
-                        <a href={item.path} className={styles.nav_text} key={item.id}>
-                            <li key={item.id} >
-                                <img src={item.iconSrc} alt="icone" />
-                                {item.title}
-                            </li>
-                        </a>
+                        <div className={router.asPath === item.path ? styles.selected : styles.not_selected}>
+                            <a href={item.path} className={styles.nav_text} >
+                                <li key={index}>
+                                    <img src={item.iconSrc} alt="icone" />
+                                    {item.title}
+                                </li>
+                            </a>
+                        </div>
                     )
                 })}
             </ul>
