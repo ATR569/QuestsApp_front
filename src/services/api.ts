beab3f92b1266a4860eead62fad35b21ed02f79d
Adson-Macedo <agnsoft@hotmail.com>
2021-04-25 21:18:58 -0300

    Cadastro de Grupos: finalizando a integração com o backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv'
 import axios from "axios"
 
-dotenv.config()
-
-const baseApiUrl = process.env.BASE_API_URL || 'http://localhost:3001'
+const baseApiUrl = 'http://localhost:3001/'
 
 export const api = axios.create({
     baseURL: baseApiUrl
