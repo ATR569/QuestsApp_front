876635dc295950b13220e9433a9e1bf5f458dfd1
Thairam <thayram01@gmail.com>
2021-05-11 22:16:32 -0300

    Ajuste no model de Questionnaires

--- src/domain/model/questionnaire.ts
+++ src/domain/model/questionnaire.ts
@@ -5,6 +5,8 @@ import { Question } from './question'
 export class Questionnaire extends Entity implements IJSONTransformable<Questionnaire> {
     private _discipline?: string
     private _questions?: Array<Question>
+    private _groupId?: string
+    private _questionsCount?: number
 
     public get discipline(): string | undefined {
         return this._discipline
@@ -22,28 +24,42 @@ export class Questionnaire extends Entity implements IJSONTransformable<Question
         this._questions = questions
     }
 
+    public get groupId(): string | undefined {
+        return this._groupId
+    }
+
+    public set groupId(groupId: string | undefined) {
+        this._groupId = groupId
+    }
+
+    public get questionsCount(): number | undefined {
+        return this._questionsCount
+    }
+
+    public set questionsCount(questionsCount: number | undefined) {
+        this._questionsCount = questionsCount
+    }
+
     public toJSON(): object {
-        return {
+        return (this._questions) ? {
             id: this.id,
             discipline: this.discipline,
+            groupId: this.groupId,
             questions: this.questions
+        } : {
+            id: this.id,
+            discipline: this.discipline,
+            groupId: this.groupId,
+            questionsCount: this.questionsCount
         }
     }
 
     public fromJSON(json: any): Questionnaire {
-        if (json === undefined) {
-            json = {}
-        }
         if (json.id !== undefined) this.id = json.id
         if (json.discipline !== undefined) this.discipline = json.discipline
-        if (json.questions !== undefined && json.questions instanceof Array) {
-            this.questions = json.questions.map((question: any) => new Question().fromJSON(question))
-        }
+        if (json.questions !== undefined) this.questions = json.questions
+        if (json.groupId !== undefined) this.groupId = json.groupId
 
         return this
-    }
-
-    public getQuestionsCount(): number {
-        return this.questions ? this.questions.length : 0
     }
 }
