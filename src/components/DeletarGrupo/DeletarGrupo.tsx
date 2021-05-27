6a306c276b42532c4a4844b6db99bf1b99cb6515
Thairam <thayram01@gmail.com>
2021-05-26 22:15:12 -0300

    Deletar grupo de estudos

---
+++ src/components/DeletarGrupo/DeletarGrupo.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styles from './DeletarGrupo.module.css'
+import RoundedButton, { ButtonKind } from '../base/RoundedButton'
+import { Modal } from 'antd'
+import { api } from '../../services/api'
+import { openErrorNotification, openSuccessNotification } from '../../utils/notification'
+import { Group } from '../../domain/model/group'
+
+const URI = 'groups'
+
+interface IDeletarGrupoProps {
+    visible: boolean
+    setVisible: (value: boolean) => void
+    group: Group
+}
+
+const DeletarGrupo: React.FC<IDeletarGrupoProps> = ({ visible, setVisible, group }) => {
+
+    const handleCancelar = (event: Event) => {
+        event.preventDefault()
+        setVisible(false)
+    }
+
+    const handleExcluir = async (event: Event) => {
+        event.preventDefault()
+        setVisible(true)
+
+        await api.delete(URI.concat(`/${group.id}`))
+            .then((res: any) => {
+                openSuccessNotification('Deletado com sucesso!')
+                setVisible(false)
+                setTimeout(() => window.location.reload(), 1000)
+            })
+            .catch((err: any) => {
+                openErrorNotification(err.response.data)
+            })
+    }
+
+    return (
+        <Modal
+            className={styles.modal}
+            title={<div className={styles.title}> Deletar Grupo </div>}
+            closable={false}
+            centered
+            visible={visible}
+            footer="">
+
+            <div className={styles.content}>
+                <div className={styles.form}>
+                    <h3 style={{ display: 'flex', justifyContent: 'center', padding: '10px', color: 'var(--orange)', fontWeight: 'bold' }}>
+                        {group.name}
+                    </h3>
+                    <div className={styles.buttons}>
+                        <RoundedButton
+                            label="Cancelar"
+                            buttonKind={ButtonKind.CancelButton}
+                            width="120px"
+                            onClick={handleCancelar} />
+
+                        <RoundedButton
+                            label="Deletar"
+                            buttonKind={ButtonKind.ConfirmButton}
+                            width="120px"
+                            onClick={handleExcluir}
+                        />
+                    </div>
+                </div >
+            </div>
+
+        </Modal>
+    )
+}
+
+export default DeletarGrupo
--- src/components/base/CardGroup.tsx
+++ src/components/base/CardGroup.tsx
@@ -1,36 +1,96 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { Group } from '../../domain/model/group'
-
+import { AuthService } from '../../services/auth'
+import DeletarGrupo from '../DeletarGrupo/DeletarGrupo'
 import styles from './CardGroup.module.css'
 
 interface IGroupProps {
     group: Group
 }
 
-export default class CardGroup extends Component<IGroupProps, {}> {
+const CardGroup: React.FC<IGroupProps> = ({ group }) => {
 
-    render(): any {
+    const [visible, setVisible] = useState(false)
+    const [loggedUserId, setLoggedUserId] = useState('')
 
-        const group: Group = this.props.group
+    useEffect(() => {
+        const user = AuthService.decodeToken().user
+        setLoggedUserId(user.id)
+    }, [])
 
-        return (
-            <div className={styles.card_group}>
-
-                <div className={styles.container_img}>
-                    <img src='icons/group.svg' className={styles.img}></img>
-                </div>
-
-                <div className={styles.title_group}>
-                    {group.name}
-                </div>
-
-                <div className={styles.informations}>
-                    <span>{`${group.membersCount} Membros`}</span>
-                    <span>{`${group.questionnairesCount} Questionários`}</span>
-                </div>
-
-                <div className={styles.effect} />
-            </div>
-        )
+    const handleDelete = (event: any) => {
+        event.preventDefault()
+        setVisible(true)
     }
+
+    function checkAdmin(groupAdministratorId: string): boolean {
+        return groupAdministratorId === loggedUserId
+    }
+
+    return (
+        <div className={styles.card_group}>
+            <DeletarGrupo visible={visible} setVisible={setVisible} group={group} />
+
+            <div className={styles.del_group} >
+                <button
+                    className={styles.buttons}
+                    onClick={e => handleDelete(e)}
+                    style={checkAdmin(group.administrator.id) ? {} : { visibility: 'hidden' }} >
+                    <img src='icons/fechar.svg' className={styles.img_trash}></img>
+                </button>
+            </div>
+
+            <div className={styles.container_img}>
+                <img src='icons/group.svg' className={styles.img}></img>
+            </div>
+
+            <div className={styles.title_group}>
+                {group.name}
+            </div>
+
+            <div className={styles.informations}>
+                <span>{`${group.membersCount} Membros`}</span>
+                <span>{`${group.questionnairesCount} Questionários`}</span>
+            </div>
+
+            <div className={styles.effect} />
+
+        </div>
+    )
 }
+
+export default CardGroup
+
+// export default class CardGroup extends Component<IGroupProps, {}> {
+
+//     render(): any {
+
+//         const group: Group = this.props.group
+
+//         return (
+//             <div className={styles.card_group}>
+
+//                 <div className={styles.del_group} >
+//                     <button className={styles.buttons}>
+//                         <img src='icons/fechar.svg' className={styles.img_trash}></img>
+//                     </button>
+//                 </div>
+
+//                 <div className={styles.container_img}>
+//                     <img src='icons/group.svg' className={styles.img}></img>
+//                 </div>
+
+//                 <div className={styles.title_group}>
+//                     {group.name}
+//                 </div>
+
+//                 <div className={styles.informations}>
+//                     <span>{`${group.membersCount} Membros`}</span>
+//                     <span>{`${group.questionnairesCount} Questionários`}</span>
+//                 </div>
+
+//                 <div className={styles.effect} />
+//             </div>
+//         )
+//     }
+// }
